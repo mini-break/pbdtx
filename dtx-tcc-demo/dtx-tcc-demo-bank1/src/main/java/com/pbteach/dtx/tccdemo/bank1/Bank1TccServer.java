b21123ce0b464647a3aa84156d7c6caca6279144
mini-break <mini_break@163.com>
2020-07-30 22:15:48 +0800

    分布式事务测试

--- dtx-tcc-demo/dtx-tcc-demo-bank1/src/main/java/com/pbteach/dtx/tccdemo/bank1/Bank1TccServer.java
+++ dtx-tcc-demo/dtx-tcc-demo-bank1/src/main/java/com/pbteach/dtx/tccdemo/bank1/Bank1TccServer.java
@@ -4,13 +4,15 @@ package com.pbteach.dtx.tccdemo.bank1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class}) // 排除启动时mongodb报错
 @EnableDiscoveryClient
 @EnableHystrix
 @EnableFeignClients(basePackages = {"com.pbteach.dtx.tccdemo.bank1.spring"})
--- dtx-tcc-demo/dtx-tcc-demo-bank2/src/main/java/com/pbteach/dtx/tccdemo/bank2/Bank2TccServer.java
+++ dtx-tcc-demo/dtx-tcc-demo-bank2/src/main/java/com/pbteach/dtx/tccdemo/bank2/Bank2TccServer.java
@@ -4,12 +4,14 @@ package com.pbteach.dtx.tccdemo.bank2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableHystrix
 @ComponentScan({"com.pbteach.dtx.tccdemo.bank2","org.dromara.hmily"})
